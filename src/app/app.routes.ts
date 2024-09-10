3bdd132e58966da1cbcec158b60f6dfaafb5ec28
kunal kirola <kunalkirola@gmail.com>
2024-09-10 09:27:11 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Component1Component } from './components/component1/component1.component';
+import { Component2Component } from './components/component2/component2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { redirectTo: 'c1', pathMatch: 'full', path: '' },
+  { path: 'c1', component: Component1Component },
+  { path: 'c2', component: Component2Component },
+];
